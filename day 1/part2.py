372f099a1c578d5f6cd0847d3ab4a29602f8941e
Arthur Carrell <asteristired@gmail.com>
2022-12-01 12:31:04 +0000

    simple fix
    i forgo💀

--- day 1/part2.py
+++ day 1/part2.py
@@ -30,11 +30,11 @@ for item in range(sum(1 for line in open("day 1\input.txt","r"))):
                 print(f"Lower or equal to second highest answer. ({answer[1]} > {currentAnswer})")
                 if currentAnswer > answer[2]:
                     answer[2] = currentAnswer
-                    print("This is higher then the second current answer, this is the new answer.")
+                    print("This is higher then the third current answer, this is the new answer.")
                 else:
                     print(f"Lower or equal to third highest answer. ({answer[2]} > {currentAnswer})") 
         currentAnswer = 0
 print(f"\nAnswer 1: {answer[0]}")
 print(f"Answer 2: {answer[1]}")
 print(f"Answer 3: {answer[2]}")
-print(f"The total sum is {int(answer[0]) + int(answer[1]) + int(answer[2])}!")
+print(f"The total sum is {int(answer[0]) + int(answer[1]) + int(answer[2])}!")  
